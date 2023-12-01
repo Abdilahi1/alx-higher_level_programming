cfd8cb2eda11f90df58506710bf19ec3e7c89e01
Abdilahi1 <78045297+Abdilahi1@users.noreply.github.com>
2023-12-02 01:37:55 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-"""Script for fetching url https://alx-intranet.hbtn.io/status"""
+"""A script that
+- fetches https://alx-intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-from urllib import request
 
+if __name__ == '__main__':
+    import urllib.request
 
-if __name__ == "__main__":
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
-        html = resp.read()
-
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-          .format(type(html), html, html.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
